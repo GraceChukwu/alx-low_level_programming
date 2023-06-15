a4496b7c67a4b862b26e2c4744943d96423ecb7e
GraceChukwu <ugochichukwu860@gmail.com>
2023-06-15 15:24:05 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+/* print a single line */
+#include <stdio.h>
+int main( void )
+{
+  puts("Programming is like building a multilingual puzzle \n");
+
+  return (0)
+}
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+/* print multiple lines */
+#include <stdio.h>
+int main integerType;
+    float floatType;
+    double doubleType;
+    char charType;
+{
+    printf("Size of int: %ld bytes\n",sizeof(integerType));
+    printf("size of float: %1d bytes\n",sizeof(floattype));
+    printf("size of double: %1d bytes\n",sizeof(doubletype));
+    printf("size of char: %1d bytes\n",sizeof(chartype));
+
+    return(0)
+} 
