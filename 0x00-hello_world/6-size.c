636076c406fbbc720ab7ca760fe93ae4892107f4
GraceChukwu <ugochichukwu860@gmail.com>
2023-06-17 17:04:28 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,19 @@
-/* print multiple lines */
 #include <stdio.h>
-int main integerType;
-    float floatType;
-    double doubleType;
-    char charType;
+/**
+ * main - A program that prints multiple line with printf function
+ * return: 0 (success)
+ */
+int main(void)
 {
-    printf("Size of int: %ld bytes\n",sizeof(integerType));
-    printf("size of float: %1d bytes\n",sizeof(floattype));
-    printf("size of double: %1d bytes\n",sizeof(doubletype));
-    printf("size of char: %1d bytes\n",sizeof(chartype));
-
-    return(0)
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("Size of a char: %lu bytes\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu bytes\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu bytes\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu bytes\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu bytes\n", (unsigned long)sizeof(f));
+return (0);
 } 
