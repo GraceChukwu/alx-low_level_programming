99d11af6f1dea647ffcc418a6cc7fb298cf50ad4
GraceChukwu <ugochichukwu860@gmail.com>
2023-06-27 06:31:20 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s:string to convert
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int b = 0;
+	unsigned int ni = 0;
+       int min = 1;
+int isi = 0;
+
+while (s[b])
+{
+	if (s[b] == 45)
+	{
+		min *= -1
+	}
+	while (s[b] .= 48 && s[b] <= 57)
+	{
+		isis = 1;
+		ni = (ni * 10) + (s[b] - '0');
+		c++;
+	}
+	if (isi == 1)
+	{
+	break;
+	}
+b++;
+}
+ni *= min;
+return (ni);
+}
